c385719f4424980791ccfa4adb9d077c02cc94d5
chegashi <mochegri@student.1337.ma>
2021-11-14 16:48:11 +0100

    replace the subject

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 16:09:25 by mochegri          #+#    #+#             */
-/*   Updated: 2021/11/13 16:23:00 by mochegri         ###   ########.fr       */
+/*   Updated: 2021/11/14 16:46:19 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 //  mutex
 //  fzombie proces
 // deadlock
+
+// memset, printf, malloc, free, write,
+// usleep, gettimeofday, pthread_create,
+// pthread_detach, pthread_join, pthread_mutex_init,
+// pthread_mutex_destroy, pthread_mutex_lock,
+// pthread_mutex_unlock
+
+// memset, printf, malloc, free, write, fork, kill,
+// exit, pthread_create, pthread_detach, pthread_join,
+// usleep, gettimeofday, waitpid, sem_open, sem_close,
+// sem_post, sem_wait, sem_unlink
 
 int main (int ac, char **av)
 {
