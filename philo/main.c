0b50f35b1871740d62a20b85b66a41ec040f0f40
chegashi <mochegri@student.1337.ma>
2021-12-06 15:14:19 +0100

    init parssing

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 16:09:25 by mochegri          #+#    #+#             */
-/*   Updated: 2021/11/14 16:46:19 by mochegri         ###   ########.fr       */
+/*   Updated: 2021/12/05 18:24:08 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,68 @@
 // usleep, gettimeofday, waitpid, sem_open, sem_close,
 // sem_post, sem_wait, sem_unlink
 
+#include "philo.h"
+
 int main (int ac, char **av)
 {
-    (void)ac;
-    (void)av;
-    return (0);
+	t_philo *philosophers;
+
+	philosophers = init_philosophers(ac, av);
+	if (!philosophers)
+		return (ft_perror());
+	
+	return (0);
+}
+
+t_philo	*init_philosophers(int ac, char **av)
+{
+	t_philo *philosophers;
+
+	if (ac < 4 || ac > 5)
+		return (NULL);
+	philosophers->nbr_of_philo = ft_atoi(*(av + 1));
+	philosophers->time_to_die = ft_atoi(*(av + 2));
+	philosophers->time_to_eat =  ft_atoi(*(av + 3));
+	philosophers->time_to_sleep =  ft_atoi(*(av + 4));
+	if (av + 5)
+		philosophers->nbr_of_meals = ft_atoi(*(av + 5));
+	else
+		philosophers->nbr_of_meals = 0;
+	if (philosophers->nbr_of_philo < 0 || philosophers->time_to_die < 0 ||
+		philosophers->time_to_eat < 0 || philosophers->time_to_sleep < 0||
+		philosophers->nbr_of_meals < 0)
+		return (NULL);
+	return (philosophers);
+}
+
+int ft_perror(void)
+{
+	printf("error\n");
+	return (1);
+}
+
+int	ft_atoi(char *s)
+{
+	long	nbr;
+	int		signe;
+
+	signe = 1;
+	nbr = 0;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			signe = -1;
+		s++;
+	}
+	while (*s)
+	{
+		nbr = nbr * 10 + (*s - '0');
+		if (*s < '0' || *s > '9' || (signe == 1 && nbr > 2147483647)
+			|| (signe == -1 && nbr > 2147483648))
+			return (-1);
+		s++;
+	}
+	nbr *= signe;
+	return ((int)nbr);
 }
 
